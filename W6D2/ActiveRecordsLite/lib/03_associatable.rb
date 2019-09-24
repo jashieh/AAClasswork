117c666e60617092c19459603bbc79d60e53a632
Justin Shieh <shieh_justin@yahoo.com>
2019-09-24 16:31:43 -0700

    added searchable module

--- W6D2/ActiveRecordsLite/lib/03_associatable.rb
+++ W6D2/ActiveRecordsLite/lib/03_associatable.rb
@@ -21,12 +21,32 @@ end
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
     # ...
+     defaults = {
+      :foreign_key => "#{name}_id".to_sym,
+      :class_name => name.to_s.camelcase,
+      :primary_key => :id
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
+
+
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
     # ...
+    defaults = {
+      :foreign_key => "#{self_class_name.underscore}_id".to_sym,
+      :class_name => name.to_s.singularize.camelcase,
+      :primary_key => :id
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
