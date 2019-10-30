435b62d46f3df48aed8c05f1500b4e420b949b44
Justin Shieh <shieh_justin@yahoo.com>
2019-10-30 11:38:01 -0700

    added login,signup and logout for bluebird app

---
+++ W11D3/Frontend_auth/frontend/components/session/login.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+export default class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            email: "",
+            password: "",
+        };
+        this.handleInput = this.handleInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInput(type) {
+        return (e) => this.setState({ [type]: e.target.value });
+    }   
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.login(this.state)
+            .then(() => this.props.history.push('/chirps'));
+    }
+    
+    render() {
+        return(
+            <div>
+                <h2>Sign In!</h2>
+                <form>
+                    <label>Username:
+                        <input 
+                            onChange={this.handleInput('username')} 
+                            type="text" 
+                            value={this.state.username}/>
+                    </label>
+                </form>
+
+                <form>
+                    <label>Email:
+                        <input 
+                            onChange={this.handleInput('email')} 
+                            type="text" 
+                            value={this.state.email}/>
+                    </label>
+                </form>
+
+                <form>
+                    <label>Password:
+                        <input 
+                            onChange={this.handleInput('password')} 
+                            type="password" 
+                            value={this.state.password}/>
+                    </label>
+                </form>
+
+                <input onClick={this.handleSubmit} type="submit" />
+            </div>
+        );      
+    }
+}
